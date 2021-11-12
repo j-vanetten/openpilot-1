9ee3c4ed5d33cd7e3ce8d27c5efe4c7db3e319a6
John <4vanetten@gmail.com>
2021-11-11 19:53:20 -0700

    Revert "remove unused stuff"
    This reverts commit 3c0a7e18a03d5b7c69e9323fcfb0df71d25cad62.

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -4,7 +4,10 @@ from opendbc.can.can_define import CANDefine
 from selfdrive.config import Conversions as CV
 from selfdrive.car.interfaces import CarStateBase
 from selfdrive.car.chrysler.values import DBC, STEER_THRESHOLD
+from common.cached_params import CachedParams
+from common.op_params import opParams
 from selfdrive.car.chrysler.values import CAR
+import numpy as np
 
 ButtonType = car.CarState.ButtonEvent.Type
 
@@ -25,6 +28,8 @@ class CarState(CarStateBase):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]["pt"])
     self.shifter_values = can_define.dv["GEAR"]["PRNDL"]
+    self.cachedParams = CachedParams()
+    self.opParams = opParams()
     self.lkasHeartbit = None
     self.dashboard = None
     self.speedRequested = 0
@@ -36,7 +41,7 @@ class CarState(CarStateBase):
     self.hybrid = CP.carFingerprint in (CAR.PACIFICA_2017_HYBRID, CAR.PACIFICA_2018_HYBRID, CAR.PACIFICA_2019_HYBRID)
 
   def update(self, cp, cp_cam):
-    min_steer_check = False
+    min_steer_check = self.opParams.get('steer.checkMinimum')
 
     ret = car.CarState.new_message()
 
